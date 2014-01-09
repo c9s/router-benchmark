5cc4302d1eb9af9c7345de2c67bf0a3df57e90d3
c9s <cornelius.howl@gmail.com>
2014-01-09 13:13:27 +0800

    update staticRoutes cache

--- code/pux/hello_mux.php
+++ code/pux/hello_mux.php
@@ -15,6 +15,17 @@
     ),
   ),
    'submux' => array(),
-   // 'staticRoutes' => array(),
+   'staticRoutes' => array(
+       '/hello' => array (
+            0 => false,
+            1 => '/hello',
+            2 => 
+            array (
+                0 => 'HelloController',
+                1 => 'helloAction',
+            ),
+            3 => array (),
+            ),
+   ),
    'expand' => true,
 )); /* version */
