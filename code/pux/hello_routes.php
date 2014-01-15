330e54b7f29723d63a9cb0cd672091e18cbb4a62
c9s <cornelius.howl@gmail.com>
2014-01-15 09:01:16 +0800

    revert

--- code/pux/hello_routes.php
+++ code/pux/hello_routes.php
@@ -1,6 +1,6 @@
 <?php
 // pux has zero dependency, so we even don't use composer autoloader.
-use pux\Mux;
+use Pux\Mux;
 $mux = new Mux;
 $mux->add('/hello', ['HelloController','helloAction']);
 return $mux;
